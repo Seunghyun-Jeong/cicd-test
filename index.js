c92e6a31f389be8725395d94e33b1f4918547dd2
Seunghyun-Jeong <dkskakdgka@gmail.com>
2023-06-16 16:53:28 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+    res.end("Git -> Jenkins -> ECR -> ECS CI/CD 테스트입니다.\n");
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
